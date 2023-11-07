81c82858076169ca77998300f2d2ddfa80487b0d
GritsaevTim <timgri93@gmail.com>
2023-11-07 20:56:54 +0300

    da

--- src/collate_fn/collate.py
+++ src/collate_fn/collate.py
@@ -12,8 +12,10 @@ def collate_fn(dataset_items: List[dict]):
     """
     y_wav, x_wav, x_wav_len, target_wav, speaker_id = [], [], [], [], []
 
-    def get_max_length(key):
-        return max(dataset_items, key=lambda item: item[key].shape[1]).shape[1]
+    print(dataset_items)
+
+    def get_max_length(key_):
+        return max(dataset_items, key=lambda item: item[key_].shape[1]).shape[1]
 
     def pad_to_len(wav, len):
         return F.pad(wav, (1, len - wav.shape[1]))
