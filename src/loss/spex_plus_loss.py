199ed7998e4a562fc201bf5666c21fbed4d346fe
GritsaevTim <timgri93@gmail.com>
2023-11-07 13:33:27 +0300

    da

--- src/loss/spex_plus_loss.py
+++ src/loss/spex_plus_loss.py
@@ -20,6 +20,14 @@ class SpExPlusLoss(nn.Module):
         self.ce_loss = nn.CrossEntropyLoss()
 
     def forward(self, s1, s2, s3, speaker_pred, target_wav, speaker_id, **kwargs):
-        loss_si_sdr = -((1 - self.alpha - self.beta) * si_sdr(s1, target_wav) + self.alpha * si_sdr(s2, target_wav) + self.beta * si_sdr(s3, target_wav))
+        s1 -= s1.mean(axis=-1)
+        s2 -= s2.mean(axis=-1)
+        s3 -= s3.mean(axis=-1)
+        target_wav -= target_wav.mean(axis=-1)
+
+        batch_size = target_wav.shape[0]
+        loss_si_sdr = (
+            -(1 - self.alpha - self.beta) * si_sdr(s1, target_wav).sum() - self.alpha * si_sdr(s2, target_wav).sum() - self.beta * si_sdr(s3, target_wav).sum()
+        ) / batch_size
         ce = self.ce_loss(speaker_pred, speaker_id.to(speaker_pred.device))
         return loss_si_sdr + self.gamma * ce
