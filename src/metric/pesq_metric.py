fab84548b402f60e67184cc24125b6f18a35d792
GritsaevTim <timgri93@gmail.com>
2023-11-07 13:05:26 +0300

    da

--- src/metric/pesq_metric.py
+++ src/metric/pesq_metric.py
@@ -3,8 +3,8 @@ from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality
 
 
 class PESQMetric(BaseMetric):
-    def __init__(self, fs=16000, mode="wb"):
-        super().__init__()
+    def __init__(self, fs=16000, mode="wb", *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.metric = PerceptualEvaluationSpeechQuality(fs, mode)
 
     def __call__(self, preds, target):
