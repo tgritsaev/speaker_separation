7300ee9c2b85146867278871c68faec412d31dff
GritsaevTim <timgri93@gmail.com>
2023-11-08 23:52:25 +0300

    da

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -71,7 +71,7 @@ class Trainer(BaseTrainer):
         if is_train:
             self.optimizer.zero_grad()
 
-        with torch.autocast(device_type=self.device.type, dtype=torch.float16):
+        with torch.autocast(device_type=self.device.type):
             outputs = self.model(**batch)
             batch.update(outputs)
             if is_train:
