b326fd12bf451afa0400314f55b9f3952f630c2f
GritsaevTim <timgri93@gmail.com>
2023-11-10 21:48:24 +0300

    da

--- test.py
+++ test.py
@@ -23,6 +23,7 @@ def vad_merge(w, top_db=25):
         for i in range(1, len(intervals)):
             left, right = intervals[i - 1, 0], intervals[i, 1]
             w[left:right] = 0
+        print(w.shape)
     return w
 
 
