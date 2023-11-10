3c71601c84b9596f5f7a052ffb46f22662cbf49e
GritsaevTim <timgri93@gmail.com>
2023-11-10 17:08:48 +0300

    da

--- src/metric/si_sdr_metric.py
+++ src/metric/si_sdr_metric.py
@@ -1,12 +1,11 @@
-import torch
 from src.base.base_metric import BaseMetric
+from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
 
 
 class SISDRMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.eps = 1e-6
+        self.metric = ScaleInvariantSignalDistortionRatio()
 
     def __call__(self, s1, target_wav, **kwargs):
-        alpha = (target_wav * s1).sum() / (torch.linalg.norm(target_wav) ** 2)
-        return 20 * torch.log10(torch.linalg.norm(alpha * target_wav) / (torch.linalg.norm(alpha * target_wav - s1) + self.eps) + self.eps)
+        return self.metric.to(s1.device)(s1, target_wav).item()
