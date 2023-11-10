a81020c3d34f36b1ce9b800b1edfc0e82e32e459
GritsaevTim <timgri93@gmail.com>
2023-11-10 21:24:01 +0300

    da

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ from src.utils.parse_config import ConfigParser
 
 
 def vad_merge(w, top_db=20):
-    intervals = librosa.effects.split(w, top_db=top_db)
+    intervals = librosa.effects.split(w.cpu(), top_db=top_db)
     print(intervals)
     for i in range(1, len(intervals)):
         left, right = intervals[i - 1, 0], intervals[i, 1]
