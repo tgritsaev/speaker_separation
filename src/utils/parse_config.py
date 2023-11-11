075c04e2629dd6c7d271caa4a3a137ff133e1e46
GritsaevTim <timgri93@gmail.com>
2023-11-11 12:34:30 +0300

    da

--- src/utils/parse_config.py
+++ src/utils/parse_config.py
@@ -67,6 +67,10 @@ class ConfigParser:
             os.environ["CUDA_VISIBLE_DEVICES"] = args.device
         if args.resume is not None:
             resume = Path(args.resume)
+            if args.config is not None:
+                cfg_fname = Path(args.config)
+            else:
+                cfg_fname = resume.parent / "config.json"
         else:
             msg_no_cfg = "Configuration file need to be specified. " "Add '-c config.json', for example."
             assert args.config is not None, msg_no_cfg
