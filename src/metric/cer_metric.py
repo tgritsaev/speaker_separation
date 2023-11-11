f9400aaaf242cc7bc32b72237763a2c968b35102
GritsaevTim <timgri93@gmail.com>
2023-11-11 17:32:10 +0300

    da

--- src/metric/cer_metric.py
+++ src/metric/cer_metric.py
@@ -14,17 +14,19 @@ class ArgmaxCERMetric(BaseMetric):
         self.text_encoder = text_encoder
         self.name = name
 
-    def __call__(self, pred_log_probs: Tensor, target_log_probs: Tensor, text: List[str], **kwargs):
+    def __call__(self, pred_log_probs: Tensor, target_log_probs: Tensor, pred_lengths, target_lengths, text: List[str], **kwargs):
         cers = []
         if "target" in self.name:
             predictions = torch.argmax(target_log_probs.cpu(), dim=-1).numpy()
+            lengths = pred_lengths
         else:
             predictions = torch.argmax(pred_log_probs.cpu(), dim=-1).numpy()
-        for log_prob_vec, target_text in zip(predictions, text):
+            lengths = target_lengths
+        for log_prob_vec, length, target_text in zip(predictions, lengths, text):
             target_text = BaseTextEncoder.normalize_text(target_text)
             if hasattr(self.text_encoder, "ctc_decode"):
-                pred_text = self.text_encoder.ctc_decode(log_prob_vec)
+                pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             else:
-                pred_text = self.text_encoder.decode(log_prob_vec)
+                pred_text = self.text_encoder.decode(log_prob_vec[:length])
             cers.append(calc_cer(target_text, pred_text))
         return sum(cers) / len(cers)
